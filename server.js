8f9093516594361177ab775ae90247a54afcce44
GuyRimel <53277832+GuyRimel@users.noreply.github.com>
2022-09-05 17:12:24 -0500

    debugging server.js

--- server.js
+++ server.js
@@ -1,41 +1,74 @@
-const http = require("http"),
-  url = require("url"),
-  fs = require("fs");
+// lines 3 through 36 have an unknown bug... 
+
+// const http = require("http"),
+//       url = require("url"),
+//       fs = require("fs");
+
+// http.createServer((request, response) => {
+//   let addr = request.url,
+//       q = url.parse(addr, true),
+//       filePath = '';
+    
+//   fs.appendFile('log.txt', 'URL: ' + addr + '\nTimestamp: ' + new Date() + '\n\n', (err) => {
+//     if (err) {
+//       console.log(err);
+//     } else {
+//       console.log('Added to log.');
+//     }
+//   });
+    
+//   if( q.pathName.includes('documentation') ) {
+//     filePath = (__dirname + '/documentation.html');
+//   } else {
+//     filePath = 'index.html';
+//   }
+
+//   fs.readFile(filePath, (err, data) => {
+//     if(err) { throw err; }
+
+//     response.writeHead(200, { 'Content-Type': 'txt/html' });
+//     response.write(data);
+//     response.end();
+
+//   });
+
+// }).listen(8080);
+// console.log("My first Node test server is running on Port 8080.");
+
+// lines 39 through 74 work flawlessly... bc they are copy/pasted directly from the exercise!!!
+const http = require('http'),
+  fs = require('fs'),
+  url = require('url');
 
 http.createServer((request, response) => {
   let addr = request.url,
     q = url.parse(addr, true),
     filePath = '';
 
-  fs.appendFile(
-    `URL: ${addr}
-    Timestamp: ${new Date()}
-    
-    `, (err) => {
-      if(err) {
-        console.log(err);
-      } else {
-        console.log('Added to log.');
-      }
+  fs.appendFile('log.txt', 'URL: ' + addr + '\nTimestamp: ' + new Date() + '\n\n', (err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log('Added to log.');
     }
-  )
+  });
 
-  if( q.pathName.includes('documentation') ) {
-    filePath = (__dirname + 'documentation.html');
+  if (q.pathname.includes('documentation')) {
+    filePath = (__dirname + '/documentation.html');
   } else {
-    filePath = __dirname + 'index.html';
+    filePath = 'index.html';
   }
 
   fs.readFile(filePath, (err, data) => {
-    if(err) {
+    if (err) {
       throw err;
     }
 
-    response.writeHead(200, { 'Content-Type': 'txt/html' });
+    response.writeHead(200, { 'Content-Type': 'text/html' });
     response.write(data);
     response.end();
 
   });
 
 }).listen(8080);
-console.log("My first Node test server is running on Port 8080.");
+console.log('My test server is running on Port 8080.');
