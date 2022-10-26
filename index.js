9a6d075e9bb4091a0e65839dc1a5e9f4c8eae308
GuyRimel <135guyrimel@gmail.com>
2022-10-26 08:43:10 -0500

    tweaking CORS origins

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ mongoose.connect(process.env.CONNECTION_URI, {
 
 // CORS //////////
 // cross-origin resource sharing eg. accepting requests from  the frontend
-let allowedOrigins = ['http://localhost:1234'];
+let allowedOrigins = ['http://localhost:1234', 'https://m.imdb.com'];
 app.use(cors({
   origin: (origin, callback) => {
     if(!origin) return callback(null, true);
