d5a4941f62128ca782c28c0f8ad9cf9cdad36ac2
GuyRimel <53277832+GuyRimel@users.noreply.github.com>
2023-03-28 08:48:12 -0500

    added route to return one movie by _id

--- index.js
+++ index.js
@@ -113,12 +113,28 @@ app.get(
   }
 );
 
-// Returns a JSON object of a single movie
+// Returns a JSON object of a single movie by title
 app.get(
   "/movies/:Title",
   passport.authenticate("jwt", { session: false }),
   (req, res) => {
     Movies.findOne({ Title: req.params.Title })
+      .then((movie) => {
+        res.status(201).json(movie);
+      })
+      .catch((err) => {
+        console.error(err);
+        res.status(500).send("Error: " + err);
+      });
+  }
+);
+
+// Returns a JSON object of a single movie by _id
+app.get(
+  "/movies/:_id",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Movies.findOne({ _id: req.params._id })
       .then((movie) => {
         res.status(201).json(movie);
       })
