be9a552541517ef91ffd46c343df456a951407c9
GuyRimel <53277832+GuyRimel@users.noreply.github.com>
2023-03-28 11:35:53 -0500

    allowed for editing user password with hashing

--- index.js
+++ index.js
@@ -266,12 +266,16 @@ app.put(
   "/users/:Username",
   passport.authenticate("jwt", { session: false }),
   (req, res) => {
+    let hashedPassword;
+    if(req.body.Password) {
+      hashedPassword = Users.hashPassword(req.body.Password);
+    }
     Users.findOneAndUpdate(
       { Username: req.params.Username },
       {
         $set: {
           Username: req.body.Username,
-          Password: req.body.Password,
+          Password: hashedPassword,
           Email: req.body.Email,
           Birthday: req.body.Birthday,
         },
